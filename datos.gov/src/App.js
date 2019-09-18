8db6bb3bfc70482686274c6b49e866c7a31061da
DanielaRocha6 <d.rocha@uniandes.edu.co>
2019-09-17 19:04:46 -0500

    prueba

--- datos.gov/src/App.js
+++ datos.gov/src/App.js
@@ -1,51 +1,77 @@
 import React from 'react';
 import './App.css';
+import Navio from './navio';
 
-export default class App extends React.Component{
+export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.url = 'Ingresa url';
     this.state = {
-      url: 'Ingresa url'
+      dataNavio: []
     };
-    this.handleSubmit=this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
- 
-change() {
-  
-  console.log(`URl es: ${this.url.value}`)
-  this.setState( {
-    url : this.url.value
-  })
+  change(e) {
+
+    console.log(`URl es: ${this.url}`)
+    this.url = e.target.value;
+  }
+
+  handleSubmit(event) {
+    let termino = false;
+    event.preventDefault();
+    alert(`${this.url}`);
+    let c = 0;
+    let temp = [0];
+    while (!termino) {
+      fetch(`${this.url}?$limit=1000&$offset=${1000 * c}`)
+        .then(req => {
+          return req.json();
+        })
+        .then(data => {
+          if (data.length !== 0) {
+            console.log(data.length);
+            termino=true;
+            this.setState({dataNavio : data});
+            console.log(this.state.dataNavio);
+            // setTimeout(() => {
+            //   console.log("Esperando");
+            //   temp.concat(data);
+            //   console.log(this.state.dataNavio);
+            //   c++;
+            // }, 2000);
+
+          }
+          else {
+            termino = true;
+            // this.setState({dataNavio : temp});
+          }
+        });
+
+    }
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Visualización datos de datos.gov.co</h1>
+          <form className="form-group" onSubmit={this.handleSubmit} >
+            <input placeholder="Ingresa una url"
+              type="text"
+              onChange={this.change.bind(this)} />
+
+            <br />
+            <input type="submit" value="CONSULTAR" />
+          </form >
+        </div>
+        {this.state.dataNavio && <Navio data={this.state.dataNavio} />}
+      </div>
+    );
+  }
 }
 
-handleSubmit(event) { 
-  event.preventDefault();
-  alert(`${this.url.value}`);
-  fetch(`${this.url.value}`)
-  .then(req => {
-    return req.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
-  
-  }
- render() {
-  return (
-    <div>
-    <div>
-      <h1>Visualización datos de datos.gov.co</h1>
-      <form className="form" onSubmit={this.handleSubmit} >
-        <input placeholder="Ingresa una url" type="text" 
-        ref={ url => this.url = url}  onChange = {this.change.bind(this)}  />
-        <br/>
-        <input type="submit" value="CONSULTAR" />
-        </form >
-    </div>
-    <div id="navio"></div>
-    </div>
-  );
- 
-}
- } 
+
--- datos.gov/src/navio.js
+++ datos.gov/src/navio.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import navio from 'navio';
+import PropTypes from 'prop-types';
 
 export default class Navio extends React.Component{ 
     constructor(props) {
         super(props);
+        this.refDiv = React.createRef();
       }
-      componentDidMount() {
-          
+
+    //   componentDidMount() {
+    //       this.loadNavio();
+    //   }
+
+      componentDidUpdate() {
+          this.loadNavio();
+        // console.log(this.props.data);
+        
       }
+
       loadNavio(){
-        const nv = navio(this.mydiv, 600);
-        // NAVIO Step 2. Load your data!
-        nv.data(data);
-    
-        // NAVIO Step 3. Detect your attributes (or load them manually)
+        const nv = navio(this.refDiv.current);
+        nv.data(this.props.data);
         nv.addAllAttribs();
-    
-        // Optional, setup a selection callback
+
         nv.updateCallback( selected => console.log("selected in Navio: ", selected.length));
       }
    
+      render(){
+          return(
+            <div ref={this.refDiv}></div>
+          );
+      }
+}
+Navio.propTypes = {
+    data : PropTypes.array
 }
 
